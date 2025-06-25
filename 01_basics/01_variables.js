140b7a73e16e0fe05c4e386d2579dae309dfd02c
shivambhandari404 <sbhandari6791@gmail.com>
2025-06-25 08:02:09 +0000

    variable lesson

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+let name = "shivam"
+let email = "sbisht.com"
+let city = "alkapuri"
+let password = "4545"
+
+
+name = "ramesh"
+email = "sbhandari.com"
+city = "dehradun"
+password = "5454"
+
+console.table ([name, email, city, password])
+
